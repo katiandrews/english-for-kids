9cda3ca074de3965bbb6b4e9760338fa5b5b41c3
katiandrews <andryushina.eka@gmail.com>
2021-06-21 17:58:29 +0200

    fix: change webpack configuration

--- english-for-kids/webpack/webpack.prod.js
+++ english-for-kids/webpack/webpack.prod.js
@@ -2,10 +2,4 @@ const webpack = require('webpack')
 
 module.exports = {
   mode: 'production',
-  devtool: 'source-map',
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.name': JSON.stringify('Codevolution'),
-    }),
-  ],
 }
