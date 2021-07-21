3f793ecd76aa6b37d6f99dbfc3ccdbc4dc8dcd23
katiandrews <andryushina.eka@gmail.com>
2021-07-21 11:54:15 +0200

    refactor: change config file

--- english-for-kids/service/src/config/default.ts
+++ english-for-kids/service/src/config/default.ts
@@ -1,7 +1,7 @@
 const config = {
   port: process.env.PORT || 8081,
-  host: 'localhost',
-  jwtSecret: 'secretKey',
+  host: process.env.host,
+  jwtSecret: process.env.jwtSecret,
   dbUri: 'mongodb+srv://admin:1234qwe@cluster0.bj036.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 }
 
